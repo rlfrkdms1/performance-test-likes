2cc0391a5bbf5490b11886361a3748b780cab99b
rlfrkdms1 <rlfrkdms1@naver.com>
2024-07-20 16:45:21 +0900

    feat: 회원가입 추가

--- backend/src/main/java/test/performance/backend/member/MemberController.java
+++ backend/src/main/java/test/performance/backend/member/MemberController.java
@@ -1,7 +1,24 @@
 package test.performance.backend.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/member")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping
+    public CreateMemberResponse create() {
+        return memberService.create();
+    }
+
 }
--- backend/src/main/java/test/performance/backend/member/MemberService.java
+++ backend/src/main/java/test/performance/backend/member/MemberService.java
@@ -1,9 +1,23 @@
 package test.performance.backend.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.UUID;
 
 @Service
 @Transactional(readOnly = true)
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public CreateMemberResponse create() {
+        Member member = memberRepository.save(new Member(UUID.randomUUID().toString()));
+        return new CreateMemberResponse(member.getId());
+    }
 }
