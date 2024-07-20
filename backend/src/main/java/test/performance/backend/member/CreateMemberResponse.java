08a27bc8e07ba1690436f34ab5c9fc6c6ba6d199
rlfrkdms1 <rlfrkdms1@naver.com>
2024-07-20 16:45:30 +0900

    feat: 회원가입 dto 추가

---
+++ backend/src/main/java/test/performance/backend/member/CreateMemberResponse.java
@@ -0,0 +1,18 @@
+package test.performance.backend.member;
+
+public class CreateMemberResponse {
+
+    private Long id;
+
+    public CreateMemberResponse() {
+    }
+
+    public CreateMemberResponse(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
