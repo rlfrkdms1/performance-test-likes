dfd43ed3d814a5080965d30e0b6d3947fa1be001
rlfrkdms1 <rlfrkdms1@naver.com>
2024-07-28 22:26:04 +0900

    feat: post 조회 기능 추가

--- backend/src/main/java/test/performance/backend/post/PostController.java
+++ backend/src/main/java/test/performance/backend/post/PostController.java
@@ -1,7 +1,19 @@
 package test.performance.backend.post;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public ShowPostsResponse show() {
+        return postService.show();
+    }
 }
--- backend/src/main/java/test/performance/backend/post/PostService.java
+++ backend/src/main/java/test/performance/backend/post/PostService.java
@@ -1,9 +1,23 @@
 package test.performance.backend.post;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 public class PostService {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public ShowPostsResponse show() {
+        List<Post> posts = postRepository.findAll();
+        return ShowPostsResponse.from(posts);
+    }
 }
