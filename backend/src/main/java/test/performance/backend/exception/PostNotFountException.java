e76f215028844726426f8a460a6bb1d009dfb24f
rlfrkdms1 <rlfrkdms1@naver.com>
2024-07-29 23:26:05 +0900

    feat: 에러 메세지 추가

--- backend/src/main/java/test/performance/backend/exception/PostNotFountException.java
+++ backend/src/main/java/test/performance/backend/exception/PostNotFountException.java
@@ -1,4 +1,8 @@
 package test.performance.backend.exception;
 
 public class PostNotFountException extends RuntimeException{
+
+    public PostNotFountException() {
+        super("글을 찾을 수 없습니다.");
+    }
 }
